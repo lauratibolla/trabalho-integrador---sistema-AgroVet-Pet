54d3497a39d253ba7f2b7a96a3bcaee96df07de9
lauratibolla <138242401+lauratibolla@users.noreply.github.com>
2024-10-04 14:38:19 -0300

    Add files via upload
    arquivos iniciais (lab 5)

---
+++ CadastroAnimal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Trabalho.css';
+import Trabalho from "./Trabalho";
+
+function CadastroAnimal(){
+
+    return(
+        <div>
+           //cadastrar animal
+        </div>
+    );
+}
+
+export default Trabalho;
---
+++ CadastroTutor.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Trabalho.css';
+import Trabalho from "./Trabalho";
+
+function CadastroTutor(){
+
+    return(
+        <div>
+           //cadastrar tutor
+        </div>
+    );
+}
+
+export default Trabalho;
